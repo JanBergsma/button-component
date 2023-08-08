973d0ef2423b581771c4604a4f1c1a7c86399816
JanBergsma <58636096+JanBergsma@users.noreply.github.com>
2023-08-08 11:23:54 +0200

    installing storybook

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+export interface Props {
+  name?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  name: 'Default',
+})
+</script>
+
+<template>
+  <button>{{ name }}</button>
+</template>
+
+<style scoped></style>
