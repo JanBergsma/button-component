6b9cb43a2cd6c0764fe82e0cac65b5e0b9d31009
JanBergsma <bergsma_jan@hotmail.com>
2023-08-12 11:44:40 +0200

    backup

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -4,7 +4,7 @@ export interface Props {
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  name: 'Default',
+  name: 'Default'
 })
 </script>
 
@@ -12,4 +12,20 @@ const props = withDefaults(defineProps<Props>(), {
   <button>{{ name }}</button>
 </template>
 
-<style scoped></style>
+<style scoped>
+button {
+  padding: 0.5rem 1rem;
+  font-family: 'Noto Sans', sans-serif;
+  font-weight: 500;
+  font-size: 0.875rem;
+  border: none;
+  border-radius: 6px;
+  background: #e0e0e0;
+  box-shadow: 0px 2px 3px 0px rgba(51, 51, 51, 0.04);
+  color: #3f3f3f;
+}
+button:hover,
+button:focus {
+  background: #aeaeae;
+}
+</style>
